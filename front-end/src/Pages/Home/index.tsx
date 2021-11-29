b392a5afd991664233276a0e6ac483ddb2ac0e95
Nicolas Laptop-Solvimm <nicolas.thiesen@solvimm.com>
2021-11-28 23:13:10 -0300

    Add components Work Areas and Certified Section

--- front-end/src/Pages/Home/index.tsx
+++ front-end/src/Pages/Home/index.tsx
@@ -1,10 +1,14 @@
 import { Header } from '../../Components'
 import { Footer } from '../../Components/Footer'
+import { WorkAreas } from '../../Components';
+import { CertifiedSection} from '../../Components';
 
 const Home = () => {
     return (
         <>
             <Header></Header>
+            <WorkAreas></WorkAreas>
+            <CertifiedSection></CertifiedSection>
             <Footer></Footer>
         </>
     )
