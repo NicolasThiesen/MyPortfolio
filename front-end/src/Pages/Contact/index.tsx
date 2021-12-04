7981d7a34f1849583e7b87720bc2f5a2cadaadf0
Nicolas Laptop-Solvimm <nicolas.thiesen@solvimm.com>
2021-12-04 11:55:17 -0300

    Create contact page

---
+++ front-end/src/Pages/Contact/index.tsx
@@ -0,0 +1,14 @@
+import { Header } from '../../Components'
+import { Footer } from '../../Components/Footer'
+import {Form} from '../../Components';
+
+const Contact = () => {
+    return (
+        <>
+            <Header></Header>
+            <Form></Form>
+            <Footer></Footer>
+        </>
+    )
+}
+export default Contact;
