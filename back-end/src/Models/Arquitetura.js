341ce6fdef3c958306be85a3c1a55b6a9bc5c313
Nicolas Laptop-Solvimm <nicolas.thiesen@solvimm.com>
2021-12-17 22:01:07 -0300

    Add TranslateSchema

--- back-end/src/Models/Arquitetura.js
+++ back-end/src/Models/Arquitetura.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
+const TranslateSchema = new mongoose.Schema({
+    pt : String,
+    eng: String
+})
+
 const ArquiteturaSchema = new mongoose.Schema({
-    nome: String,
-    descricao: String,
+    name: TranslateSchema,
+    description: TranslateSchema,
     url_foto: String
 });
+
 
 module.exports = mongoose.model("Arquitetura", ArquiteturaSchema);
--- back-end/src/Models/Site.js
+++ back-end/src/Models/Site.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
+const TranslateSchema = new mongoose.Schema({
+    pt : String,
+    eng: String
+})
+
 const SiteSchema = new mongoose.Schema({
-    nome: String,
-    descricao: String,
+    name: TranslateSchema,
+    description: TranslateSchema,
     url_site: String,
-    url_foto: String
+    url_photo: String
 });
 
 module.exports = mongoose.model("Site", SiteSchema);
