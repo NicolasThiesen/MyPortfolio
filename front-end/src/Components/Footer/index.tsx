1b0e0aa600cb2b027608175f931ef2ae81bcb55b
Nicolas Laptop-Solvimm <nicolas.thiesen@solvimm.com>
2022-02-21 19:49:34 -0300

    Add social icons

--- front-end/src/Components/Footer/index.tsx
+++ front-end/src/Components/Footer/index.tsx
@@ -1,8 +1,10 @@
 import Icon from "../../Assets/Nicolas Icon.png"
 import "./style.css";
-import { Link } from 'react-router-dom'
 import CloudPractitioner from '../../Assets/Badge Cloud Practitioner.png'
 import CloudArchitect from '../../Assets/Badge Solutions Architect.png'
+import Linkedin from '../../Assets/linkedin.png'
+import Github from '../../Assets/github.png'
+
 import { i18n } from '../../translate/i18n';
 
 export const Footer = () => {
@@ -10,17 +12,25 @@ export const Footer = () => {
         <footer>
             <nav>
                 <div className="footer__column">                    
-                    <img src={Icon}></img>
+                    <img src={Icon} alt="Nícolas Medeiros Thiesen"></img>
                     <h2>Nícolas Medeiros Thiesen</h2>
                     <h3>| {i18n.t("footer.title")}</h3>
                     
                 </div>
                 
-                <div>
+                <div className="footer__column">
                     
                     <div>
                         <img src={CloudPractitioner} className="footer__badge" alt="Cloud Practiotioner" />
                         <img src={CloudArchitect} className="footer__badge" alt="Cloud Practiotioner" />
+                    </div>
+                    <div>
+                        <a href="https://github.com/NicolasThiesen/">
+                            <img className="footer__icon" src={Github} alt="Github Nícolas" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/NicolasThiesen">
+                            <img className="footer__icon" src={Linkedin} alt="Linkedin Nícolas" />
+                        </a>
                     </div>
                 </div>
             </nav>
