9ea14ab4a661de2b67d997d8d4b9e323e07c4c71
Nicolas Laptop-Solvimm <nicolas.thiesen@solvimm.com>
2022-02-22 20:02:27 -0300

    Add ModalView and Select group

--- front-end/src/Components/index.tsx
+++ front-end/src/Components/index.tsx
@@ -2,3 +2,8 @@ export { default as Header } from "./Header";
 export {WorkAreas} from "./WorkAreas";
 export { CertifiedSection } from "./Certified";
 export { Form} from "./Form";
+export { Card } from "./Card";
+export { CardGrid } from "./CardGrid";
+export { Footer } from "./Footer";
+export { ModalView } from "./Modal"
+export { SelectGroup } from "./SelectGroup"
