69fe0eed9d78989a44e6edd514aff4d5ae04a54e
Nicolas Laptop-Solvimm <nicolas.thiesen@solvimm.com>
2022-03-19 19:16:43 -0300

    Add get data

--- front-end/src/Pages/Portfolio/index.tsx
+++ front-end/src/Pages/Portfolio/index.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
 import {CardGrid, Header, Footer, SelectGroup} from '../../Components'
-import dataJson from '../../Assets/data.json';
-
+import api from '../../services/api';
 
 export const Portfolio = () => {
     const [currentData, setCurrentData] = useState([]);
-    const [keyData, setKeyData] = useState(Object.keys(dataJson)[0]);
+    const [keyData, setKeyData] = useState("architecture");
+    const [data, setData] = useState({});
 
     const handle_key = (key:string) => {
         localStorage.setItem("current_selection", key);
@@ -17,17 +17,25 @@ export const Portfolio = () => {
         if (current_key){
             setKeyData(current_key!);
         }
+        async function fetchData() {
+            const { data } = await api.get('/data.json')
+            await setData(data)
+            setCurrentData((data as any)[keyData]);
+        }
+          fetchData()
       }, [])
 
     useEffect(() => {
-        setCurrentData((dataJson as any)[keyData]);
+        if (Object.keys(data).length>0){
+            setCurrentData((data as any)[keyData]);
+        }
     
     }, [keyData])
     
     return (
         <>
             <Header></Header>
-            <SelectGroup key_list={Object.keys(dataJson)} handle_click={handle_key} ></SelectGroup>
+            <SelectGroup key_list={Object.keys(data)} handle_click={handle_key} ></SelectGroup>
             {currentData.length > 0 ? <CardGrid data={currentData}></CardGrid> : ""}
             <Footer></Footer>
         </>
