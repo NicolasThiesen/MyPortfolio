35a0e8cacd642bc0726af62e70f789b2786de480
Nicolas Laptop-Solvimm <nicolas.thiesen@solvimm.com>
2022-03-20 15:23:44 -0300

    Add Developer Associate badge

--- front-end/src/Components/Certified/index.tsx
+++ front-end/src/Components/Certified/index.tsx
@@ -1,5 +1,6 @@
 import CloudPractitioner  from '../../Assets/Badge Cloud Practitioner.png';
 import CloudArchitect  from '../../Assets/Badge Solutions Architect.png';
+import DeveloperAssociate  from '../../Assets/Badge Developer Associate.png';
 import "./style.css";
 import { i18n } from '../../translate/i18n'
 
@@ -9,7 +10,8 @@ export const CertifiedSection = () => {
             <h2>{i18n.t("badge.title")}:</h2>
             <div className="section badges">
                 <img className="section__badge" src={CloudPractitioner} alt="Cloud Practitioner" />
-                <img className="section__badge" src={CloudArchitect} alt="Cloud Architect" />
+                <img className="section__badge" src={CloudArchitect} alt="Solutions Architect Associate" />
+                <img className="section__badge" src={DeveloperAssociate} alt="Developer Associate" />
             </div>
         </div>
     )
